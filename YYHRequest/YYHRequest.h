40b29882b9e07e0fddc5e29159157c6f3ade562c
Angelo Di Paolo <angelod101@gmail.com>
2014-07-21 23:15:12 -0400

    update project to build framework target

---
+++ YYHRequest/YYHRequest.h
@@ -0,0 +1,19 @@
+//
+//  YYHRequest.h
+//  YYHRequest
+//
+//  Created by Angelo Di Paolo on 6/3/14.
+//  Copyright (c) 2014 Yayuhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YYHRequest.
+FOUNDATION_EXPORT double YYHRequestVersionNumber;
+
+//! Project version string for YYHRequest.
+FOUNDATION_EXPORT const unsigned char YYHRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YYHRequest/PublicHeader.h>
+
+
